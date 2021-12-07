369722817fea1bb7a51b35e887bb98665629bace
mlynch018 <64503572+mlynch018@users.noreply.github.com>
2021-12-07 16:23:46 -0600

    Update kid.php

--- test/kid.php
+++ test/kid.php
@@ -2,7 +2,7 @@
 
 include('config/db_connect.php');
 
-$sql = 'SELECT professionals.Fname, professionals.Mname, professionals.Lname FROM professionals, child WHERE child.ProfID = professionals.IDno GROUP BY child.ProfID;';
+$sql = 'SELECT professionals.Fname, professionals.Mname, professionals.Lname, professionals.IDno FROM professionals, child WHERE child.ProfID = professionals.IDno GROUP BY child.ProfID;';
 // get the result set (set of rows)
 $result = mysqli_query($conn, $sql);
 // fetch the resulting rows as an array
@@ -32,7 +32,7 @@ $conn->close();
 							<h5><?php echo htmlspecialchars($k['Fname']); echo " "; echo htmlspecialchars($k['Mname']); echo " ";  echo htmlspecialchars($k['Lname']) ?></h5>
 						</div>
 						<div class="card-action right-align">
-							<a class="brand-text" href="#">more info</a>
+							<a class="brand-text" href="details.php?id=<?php echo $k['IDno']?>">more info</a>
 						</div>
 					</div>
 				</div>
