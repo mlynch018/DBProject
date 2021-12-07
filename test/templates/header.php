714a24793285aab7fa1f6d615b94b12d795c385c
mlynch018 <64503572+mlynch018@users.noreply.github.com>
2021-12-06 20:59:22 -0600

    Create header.php

---
+++ test/templates/header.php
@@ -0,0 +1,27 @@
+<head>
+	<title>JLM Talent</title>
+	<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <style type="text/css">
+	  .brand{
+	  	background: #33D1FF!important;
+	  }
+  	  .brand-text{
+  		color: #33D1FF !important;
+  	  }
+  	  form{
+  	  	max-width: 1000px;
+  	  	margin:  20px auto;
+  	  	padding:  20px;
+  	  } 
+  </style>
+</head>
+<body class="grey lighten-4">
+		<nav class="white z-depth-0">
+   	<div class="container">
+    	<a href="index.php" class="brand-logo brand-text">JLM Talent</a>
+      	<ul id="nav-mobile" class="right hide-on-small-and-down">
+        	<li><a href="add.php" class="btn brand z-depth-0">Add a Professional</a></li>
+      </ul>
+    </div>
+  </nav>
