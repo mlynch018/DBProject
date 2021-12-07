8d44c12db693a324f6caf9567d1e255c21952f32
mlynch018 <64503572+mlynch018@users.noreply.github.com>
2021-12-07 13:11:05 -0600

    Update header.php

--- test/templates/header.php
+++ test/templates/header.php
@@ -22,17 +22,17 @@
     	<a href="index.php" class="brand-logo brand-text">  JLM Talent</a>
       	<ul id="nav-mobile" class="right hide-on-small-and-down">
         	<li><a href="add.php" class="btn brand z-depth-0">Add a Professional</a></li>
-        	<li><a href="add.php" class="btn brand z-depth-0">Modify a Professional</a></li>
-        	<li><a href="add.php" class="btn brand z-depth-0">Remove a Professional</a></li>
+        	<li><a href="#" class="btn brand z-depth-0">Modify a Professional</a></li>
+        	<li><a href="#" class="btn brand z-depth-0">Remove a Professional</a></li>
     </div>
   </nav>
   <nav class="white z-depth-0">
   		<div class="container">
 		<ul id="nav-mobile" class="right hide-on-small-and-down">
-        	<li><a href="add.php" class="btn brand z-depth-0">Models</a></li>
-        	<li><a href="add.php" class="btn brand z-depth-0">Actors</a></li>
-        	<li><a href="add.php" class="btn brand z-depth-0">Voice Actors</a></li>
-        	<li><a href="add.php" class="btn brand z-depth-0">Children</a></li>
+        	<li><a href="#" class="btn brand z-depth-0">Models</a></li>
+        	<li><a href="#" class="btn brand z-depth-0">Actors</a></li>
+        	<li><a href="#" class="btn brand z-depth-0">Voice Actors</a></li>
+        	<li><a href="#" class="btn brand z-depth-0">Children</a></li>
         	<li><a href="adultman.php" class="btn brand z-depth-0">Men</a></li>
         	<li><a href="adultwoman.php" class="btn brand z-depth-0">Women</a></li>
         	<li><a href="manager.php" class="btn brand z-depth-0">Managers</a></li>
