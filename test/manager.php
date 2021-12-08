57bb80964a57d7fd4857f664a3192ee8ade7180e
mlynch018 <64503572+mlynch018@users.noreply.github.com>
2021-12-08 14:42:15 -0600

    Update manager.php

--- test/manager.php
+++ test/manager.php
@@ -2,8 +2,7 @@
 
 include('config/db_connect.php');
 
-// write query for all pizzas
-$sql = 'SELECT mannames.Fname, mannames.Mname, mannames.Lname, manager.Email, manager.Area_code, Manager.Local_phone FROM manager, mannames WHERE Manager.managerID=mannames.ManID ORDER BY ManagerID';
+$sql = 'SELECT mannames.Fname, mannames.Mname, mannames.Lname, manager.Email, manager.Area_code, Manager.Local_phone, Manager.ManagerID FROM manager, mannames WHERE Manager.managerID=mannames.ManID ORDER BY ManagerID';
 // get the result set (set of rows)
 $result = mysqli_query($conn, $sql);
 // fetch the resulting rows as an array
@@ -31,11 +30,11 @@ $conn->close();
 					<div class="card z-depth-0">
 						<div class="card-content center">
 							<h5><?php echo htmlspecialchars($man['Fname']); echo " "; echo htmlspecialchars($man['Mname']); echo " ";  echo htmlspecialchars($man['Lname']) ?></h5>
+							<div><?php echo "ID: " ; echo htmlspecialchars($man['ManagerID']);?></div>
 							<div><?php echo "Email: " ; echo htmlspecialchars($man['Email']);?></div>
 							<div><?php echo "Phone: (" ; echo htmlspecialchars($man['Area_code']); echo ") "; echo htmlspecialchars($man['Local_phone']);?></div>
+							<div class="card-action right-align">
 						</div>
-						<div class="card-action right-align">
-							<a class="brand-text" href="#">more info</a>
 						</div>
 					</div>
 				</div>
