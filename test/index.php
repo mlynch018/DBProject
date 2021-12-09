dc20a6ac90534f44a0fd4a105337f643abc0d35e
mlynch018 <64503572+mlynch018@users.noreply.github.com>
2021-12-08 20:51:59 -0600

    Update index.php

--- test/index.php
+++ test/index.php
@@ -1,14 +1,13 @@
 <?php
 
 include('config/db_connect.php');
-$sql = 'SELECT * FROM PROFESSIONALS,UNIONS WHERE Unions.UnionID=Professionals.UID ORDER BY Professionals.IDno';
+$sql = 'SELECT * FROM PROFESSIONALS,UNIONS,MANAGER WHERE Unions.UnionID=Professionals.UID AND Manager.ManagerID=Professionals.ManID ORDER BY Professionals.IDno';
 // get the result set (set of rows)
 $result = mysqli_query($conn, $sql);
 // fetch the resulting rows as an array
 $profs = mysqli_fetch_all($result, MYSQLI_ASSOC);
 // free the $result from memory (good practise)
 mysqli_free_result($result);
-
 
 $conn->close();
 ?>
@@ -24,13 +23,12 @@ $conn->close();
 		<div class="row">
 
 			<?php foreach($profs as $prof){ ?>
-
 				<div class="col s6 md3">
 					<div class="card z-depth-0">
 						<div class="card-content center">
 							<h5><?php echo htmlspecialchars($prof['Fname']); echo " "; echo htmlspecialchars($prof['Mname']); echo " ";  echo htmlspecialchars($prof['Lname']) ?></h5>
 							<div><?php echo "ID: " ; echo htmlspecialchars($prof['IDno']);?></div>
-							<div><?php echo "Manager ID: " ; echo htmlspecialchars($prof['ManID']);?></div>
+							<div><?php echo "Manager Email: " ; echo htmlspecialchars($prof['Email']);?></div>
 							<div><?php echo "Union: " ; echo htmlspecialchars($prof['Union_name']);?></div>
 						</div>
 						<div class="card-action right-align">
@@ -38,7 +36,6 @@ $conn->close();
 						</div>
 					</div>
 				</div>
-
 			<?php } ?>
 
 		</div>
